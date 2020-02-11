11923c55b061e5f9b16a3292845a72db2e9237b3
YIN <yin@YINs-MacBook-Pro.local>
2020-02-11 13:10:11 +0100

    new

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup, find_packages
+
+setup(
+name='myalloy',
+version='0.1',
+author='B. Yin',
+author_email='byin87@gmail.com',
+url='#',
+description='Package_for_Computational_Metallurgy',
+license='#',
+packages=find_packages(),
+)
+
+
