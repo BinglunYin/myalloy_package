2e3f86c11d2a1233bba832df6ed461005b1a03f3
YIN <yin@YINs-MacBook-Pro.local>
2020-02-28 11:12:25 +0100

    change

--- myalloy/solute_strengthening_theory.py
+++ myalloy/solute_strengthening_theory.py
@@ -269,6 +269,14 @@ class alloy_class:
             %(ty, sigmay) )
 
 
+            if hasattr(self, 'V0'):
+                f.write('%16s \n' \
+                %('V0_alloy (Ang^3)') )
+
+                f.write('%16.8f \n\n' \
+                %(self.V0) )
+
+
             if hasattr(self, 'dV'):
                 f.write('%16s %16s %16s \n' \
                 %('cn', 'dV (Ang^3)', 'Velem (Ang^3)') )
@@ -302,58 +310,14 @@ class alloy_class:
 
 #==============================
 
-
-def a2v(a):
-    v=a**3/4
-    return v
-
-
-
-
-def fcc_database_poly():
-    # Velem, mu, nu
-    data1=np.array([
-        [a2v(3.803),  150.4,  0.26  ],  # Rh
-        [a2v(3.839),    210,  0.26  ],  # Ir
-    
-        [a2v(3.891),     44,  0.39  ],  # Pd
-        [a2v(3.924),     61,  0.38  ],  # Pt
-       
-        [a2v(3.524),     76,  0.31  ],  # Ni 
-        [a2v(3.615),     48,  0.34  ],  # Cu
-    
-        [a2v(4.085),     30,  0.37  ],  # Ag
-        [a2v(4.078),     27,  0.44  ],  # Au
-        ])
-    return data1
-
-
-def fcc_database_Cij():
-    # Velem, Cijelem
-    data1=np.array([
-        [a2v(3.824),  415,  181,  186  ],  # Rh-DFT
-        [a2v(3.872),  587,  231,  257  ],  # Ir
-        [a2v(3.518),  276,  154,  125  ],  # Ni
-
-        [a2v(3.943),  197,  150,   71  ],  # Pd
-        [a2v(3.967),  302,  224,   58  ],  # Pt
-        [a2v(3.635),  179,  119,   88  ],  # Cu
-
-        [10.901,  296.6,  171.9,  144.0  ],  # Co (Shang et al. 2010)
-        [13.941,  479.1,  223.7,  242.5  ],  # Ru (Shang et al. 2010)
-        ])
-    return data1
-
-
-
-
-
 def fcc_Vegard_strength( ROMtype, cn, param = {}):
     
+    import alloy_database as adb 
+
     if ROMtype is 'polyelem':
-        data1 = fcc_database_poly()
+        data1 = adb.fcc_elem_poly()
     elif ROMtype is 'Cijelem':
-        data1 = fcc_database_Cij()
+        data1 = adb.fcc_elem_Cij()
 
     n0 = data1.shape[0] - cn.shape[0]
     cn = np.append(cn, np.zeros(n0) )
