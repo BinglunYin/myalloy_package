7aa0cb72a97de256d51de9ecb9da6a4712786702
YIN <yin@YINs-MacBook-Pro.local>
2021-05-07 15:49:46 +0800

    add bcc

--- myvasp/vasp_create_bcc.py
+++ myvasp/vasp_create_bcc.py
@@ -74,6 +74,83 @@ def vasp_create_bcc_110(a, ncell, bp=0):
 
 
 
+
+
+
+def vasp_create_bcc_112(a, ncell, bp=0):
+    print('==> create bcc 11-2 plane: ')
+    print(a, ncell, bp)
+
+    latt = np.array([
+        [ 1.0,     0,     0],
+        [ 1/2,   1/2,   1/2],
+        [ 3/2,  -1/2,   1/2],
+    ]) *a
+
+   
+    motif = np.array([
+        [0, 0, 0],
+    ])
+
+    ncell2 = ncell.copy()
+    for i in np.arange(3):
+        ncell2[i] = ncell[ np.mod(i+2, 3) ]
+   
+    atoms = vf.create_supercell(latt, motif, ncell2)
+    atoms = vf.make_SFP_xy(atoms, i1=1)
+    atoms = vf.make_a3_ortho(atoms)
+
+
+    if bp == 33:
+        atoms.positions = atoms.positions + np.array([0, 0, 0.1])*a
+        atoms.wrap()
+
+    atoms.pos_a0 = a 
+    vf.my_write_vasp(atoms, filename='POSCAR', vasp5=True)
+
+
+
+
+
+
+def vasp_create_bcc_123(a, ncell, bp=0):
+    print('==> create bcc 12-3 plane: ')
+    print(a, ncell, bp)
+
+    latt = np.array([
+        [ 1.0,     0,     0],
+        [ 1/2,   1/2,   1/2],
+        [ 5/2,  -1/2,   1/2],
+    ]) *a
+
+   
+    motif = np.array([
+        [0, 0, 0],
+    ])
+
+    ncell2 = ncell.copy()
+    for i in np.arange(3):
+        ncell2[i] = ncell[ np.mod(i+2, 3) ]
+   
+    atoms = vf.create_supercell(latt, motif, ncell2)
+    atoms = vf.make_SFP_xy(atoms, i1=1)
+    atoms = vf.make_a3_ortho(atoms)
+
+
+    if bp == 33:
+        atoms.positions = atoms.positions + np.array([0, 0, 0.1])*a
+        atoms.wrap()
+
+    atoms.pos_a0 = a 
+    vf.my_write_vasp(atoms, filename='POSCAR', vasp5=True)
+
+
+
+
+
+
+
+
 #===================
 # examples:
 
