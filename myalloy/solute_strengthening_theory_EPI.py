0d2e3c12ef59a2c9163902d4779d10135138a393
YIN <yin@YINs-iMac.local>
2021-07-17 16:47:40 +0800

    EPI

--- myalloy/solute_strengthening_theory_EPI.py
+++ myalloy/solute_strengthening_theory_EPI.py
@@ -9,13 +9,10 @@ from myalloy import calc_elastic_constant as cec
 
 def load_Theta_fcc_partial():
     t = np.array([
-        [2, -2,  0,  0],
-        [0,  6,  0, -4],
-        [0,  0,  2, -2],
-        [0,  0,  0, 14],
-    ])
+        [2, -2],
+        [0,  6],
+        ])
     t = cec.symmetrize_matrix(t)
-    print(t)
     return t
 
 
@@ -33,38 +30,40 @@ def calc_sigma_dUss(self, b, wc, zetac, t='fcc_partial'):
     if t=='fcc_partial':
         Theta = load_Theta_fcc_partial()
 
+    nn = np.min([ nelem, t.shape[0] ])
+
 
     print('step 1')
     s2 = 0 
-    for n1 in np.arange(nelem):
-        for n2 in np.arange(nelem):
+    for n1 in np.arange(nn):
+        for n2 in np.arange(nn):
             for d1 in np.arange(shellmax):
                 for d2 in np.arange(shellmax):
 
                     s2 = s2 +1/4 * cn[n1]*cn[n2] * EPI[d1, n1, n2]*EPI[d2, n1, n2] * Theta[d1, d2]
-                    print(s2)
+    print(s2)
 
 
     print('step 2')
-    for n1 in np.arange(nelem):
-        for n2 in np.arange(nelem):
-            for n3 in np.arange(nelem):
+    for n1 in np.arange(nn):
+        for n2 in np.arange(nn):
+            for n3 in np.arange(nn):
                 for d1 in np.arange(shellmax):
                     for d2 in np.arange(shellmax):
 
                         s2 = s2 -1/2 * cn[n1]*cn[n2]*cn[n3] * EPI[d1, n1, n2]*EPI[d2, n1, n3] * Theta[d1, d2]
-                        print(s2)
+    print(s2)
 
     print('step 3')
-    for n1 in np.arange(nelem):
-        for n2 in np.arange(nelem):
-            for n3 in np.arange(nelem):
-                for n4 in np.arange(nelem):
+    for n1 in np.arange(nn):
+        for n2 in np.arange(nn):
+            for n3 in np.arange(nn):
+                for n4 in np.arange(nn):
                     for d1 in np.arange(shellmax):
                         for d2 in np.arange(shellmax):
 
                             s2 = s2 +1/4 * cn[n1]*cn[n2]*cn[n3]*cn[n4] * EPI[d1, n1, n2]*EPI[d2, n3, n4] * Theta[d1, d2]
-                            print(s2)
+    print(s2)
     
 
     sigma_dUss = np.sqrt( zetac/np.sqrt(3)/b * 4*wc/b * s2)
