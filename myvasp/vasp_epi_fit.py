be55a94d4c0279305f05e7b6449beb24ea7b795a
YIN <yin@YINs-iMac.local>
2022-07-31 22:19:48 +0800

    fit

--- myvasp/vasp_epi_fit.py
+++ myvasp/vasp_epi_fit.py
@@ -46,8 +46,8 @@ class epi_fit:
         self.plot_lepi_res_ntrain(filename=fname1, sd=sd) 
         self.plot_lepi_res_ntrain_2(filename=fname1) 
 
-        temp = self.X.shape[1]
-        self.calc_lepi_res_shellmax(ntrain=ntrain, shellmax_range=np.arange(2, temp), fname_suffix=fname_suffix) 
+        # temp = self.X.shape[1]
+        self.calc_lepi_res_shellmax(ntrain=ntrain, shellmax_range=np.arange(2, 11), fname_suffix=fname_suffix) 
         fname2 = 'lepi_res_shellmax_%s.pkl'  %(fname_suffix)
         self.calc_nag_full_slip(filename=fname2) 
         self.plot_lepi_res_shellmax(filename=fname2) 
@@ -231,43 +231,46 @@ class epi_fit:
         ax1 = create_ax1() 
         fig_xlim = [0, ntrain[-1]]
 
-        ax1[0].plot( ntrain, mE_train  /1e3 *Ntot2, '-', color='k',  label='from atomistically computed energies ')
-        ax1[0].plot( ntrain, mE_train_p/1e3 *Ntot2, '-', color='C3', label='from EPI-predicted energies')
+        for j in np.arange(1, beta.shape[1]):
+            str1 = '$d_{%d}$'  %(j)
+            ax1[0].plot( ntrain, beta[:,j], '-', label=str1 )
         ax1[0].plot( fig_xlim, [0, 0], '--', color='gray' ) 
 
-        ax1[1].plot( ntrain, sE_train  /1e3 *Ntot2, '-', color='k',  label='from atomistically computed energies ')
-        ax1[1].plot( ntrain, sE_train_p/1e3 *Ntot2, '-', color='C3', label='from EPI-predicted energies')
+
+        ax1[1].plot( ntrain, mE_train  /1e3 *Ntot2, '-', color='k',  label='from atomistically computed energies ')
+        ax1[1].plot( ntrain, mE_train_p/1e3 *Ntot2, '-', color='C3', label='from EPI-predicted energies')
+        ax1[1].plot( fig_xlim, [0, 0], '--', color='gray' ) 
+
+
+        ax1[2].plot( ntrain, sE_train  /1e3 *Ntot2, '-', color='k',  label='from atomistically computed energies ')
+        ax1[2].plot( ntrain, sE_train_p/1e3 *Ntot2, '-', color='C3', label='from EPI-predicted energies')
 
         filename2 = '%s.full_slip.txt' %(filename[0:-4])
         if os.path.exists(filename2):
             sigma_dUss_tilde = np.loadtxt(filename2)  
-            ax1[1].plot( ntrain, sigma_dUss_tilde[0,:], '-', color='C2', label='EPI-based theoretical $ \\widetilde{\\sigma}_{\\Delta U_{s-s}} $')
+            ax1[2].plot( ntrain, sigma_dUss_tilde[0,:], '-', color='C2', label='EPI-based theoretical $ \\widetilde{\\sigma}_{\\Delta U_{s-s}} $')
 
-        for j in np.arange(1, beta.shape[1]):
-            str1 = '$d_{%d}$'  %(j)
-            ax1[2].plot( ntrain, beta[:,j], '-', label=str1 )
-
-
-        ax1[0].legend( fontsize=6, loc='lower left' )       
+      
+        ax1[0].legend( fontsize=6, loc='lower left', ncol=2)       
         ax1[1].legend( fontsize=6, loc='lower left' )       
-        ax1[2].legend( fontsize=6, loc='lower left', ncol=2)       
+        ax1[2].legend( fontsize=6, loc='lower left' )       
 
         ax1[2].set_xlim( fig_xlim )
+        ax1[2].set_xlabel('$n_\\mathrm{train}$')
+
+        ax1[0].set_ylabel('EPI $V_{\\mathrm{AuNi}, d}$ (eV)')
 
         if self.epi_type == 'normal':
-            ax1[0].set_ylabel('mean of $( \\widetilde{E}_\\mathrm{tot} - \\widetilde{E}^\\mathrm{rand}_\\mathrm{tot} )$ (eV)')
-            ax1[1].set_ylabel( 'std of $( \\widetilde{E}_\\mathrm{tot} - \\widetilde{E}^\\mathrm{rand}_\\mathrm{tot} )$ (eV)')
+            ax1[1].set_ylabel('mean of $( \\widetilde{E}_\\mathrm{tot} - \\widetilde{E}^\\mathrm{rand}_\\mathrm{tot} )$ (eV)')
+            ax1[2].set_ylabel( 'std of $( \\widetilde{E}_\\mathrm{tot} - \\widetilde{E}^\\mathrm{rand}_\\mathrm{tot} )$ (eV)')
            
         elif self.epi_type == 'diff':
-            ax1[0].set_ylabel('mean of $  \\Delta( \\widetilde{E}_\\mathrm{tot} )$ (eV)')
-            ax1[1].set_ylabel( 'std of $  \\Delta( \\widetilde{E}_\\mathrm{tot} )$ (eV)')
+            ax1[1].set_ylabel('mean of $  \\Delta( \\widetilde{E}_\\mathrm{tot} )$ (eV)')
+            ax1[2].set_ylabel( 'std of $  \\Delta( \\widetilde{E}_\\mathrm{tot} )$ (eV)')
 
-            ax1[0].set_ylim([-0.02, 0.02 ]) 
-            ax1[1].set_ylim([0, 0.06]) 
-            ax1[2].set_ylim([-0.06, 0.04]) 
-
-        ax1[2].set_ylabel('EPI $V_{\\mathrm{AuNi}, d}$ (eV)')
-        ax1[2].set_xlabel('$n_\\mathrm{train}$')
+            ax1[0].set_ylim([-0.06, 0.04]) 
+            ax1[1].set_ylim([-0.02, 0.02 ]) 
+            ax1[2].set_ylim([0, 0.06]) 
 
         vf.confirm_0( shellmax.std() )
         str1 = 'EPI $d_\\mathrm{max}=d_{%d}$' %( lepi_res[0].shellmax ) 
@@ -381,8 +384,8 @@ class epi_fit:
             ntrain     = np.append( ntrain,     lepi_res[i].ntrain     )  
             shellmax   = np.append( shellmax,   lepi_res[i].shellmax   )
             R2         = np.append( R2,         lepi_res[i].R2         )               
-            rmse_train = np.append( rmse_train, lepi_res[i].rmse_train )    
-            rmse_test  = np.append( rmse_test,  lepi_res[i].rmse_test  )    
+            rmse_train = np.append( rmse_train, lepi_res[i].rmse_train / lepi_res[i].Etrain.std() )    
+            rmse_test  = np.append( rmse_test,  lepi_res[i].rmse_test  / lepi_res[i].Etest.std()  )    
 
         vf.confirm_0( ntrain.std() )
 
@@ -391,27 +394,27 @@ class epi_fit:
 
         ax1[0].plot( shellmax, R2, '-o', color='k')
 
-        ax1[1].plot( shellmax, rmse_train, '-o', color='C0',  label='training set')
-        ax1[1].plot( shellmax, rmse_test,  '-s', color='C1',  label='testing set')
+        ax1[1].plot( shellmax, rmse_train, '-s', color='C0',  label='training set')
+        ax1[1].plot( shellmax, rmse_test,  '-o', color='C1',  label='testing set')
       
         filename2 = '%s.full_slip.txt' %(filename[0:-4])
         if os.path.exists(filename2):
             sigma_dUss_tilde = np.loadtxt(filename2)  
-            ax1[2].plot( shellmax, sigma_dUss_tilde[0,:], '-o',  color='C2', label='with all neighbours in EPIs')
-            ax1[2].plot( shellmax, sigma_dUss_tilde[1,:], '--s', color='C2', label='with first two neighbours in EPIs')
+            ax1[2].plot( shellmax, sigma_dUss_tilde[0,:], '-s', color='C2', label='with all neighbours in EPIs')
+            ax1[2].plot( shellmax, sigma_dUss_tilde[1,:], '-o', color='C4', label='with first two neighbours in EPIs')
+
 
         ax1[1].legend( fontsize=6 )       
         ax1[2].legend( fontsize=6 )       
 
         ax1[0].set_ylim([0, 1]) 
-        for i in np.arange(1, 3):
-            temp = ax1[i].get_ylim() 
-            ax1[i].set_ylim([0, np.ceil(temp[1]*10)/10 ]) 
+        ax1[1].set_ylim([0, 1]) 
+        ax1[1].set_ylim([0, 0.06]) 
 
         ax1[2].set_xlim( fig_xlim )
 
         ax1[0].set_ylabel('$R^2$')
-        ax1[1].set_ylabel('RMSE (meV/atom)')
+        ax1[1].set_ylabel('RMSE/std')
         ax1[2].set_ylabel('EPI-based theoretical $ \\widetilde{\\sigma}_{\\Delta U_{s-s}} $ (eV)')
         ax1[2].set_xlabel('$d_\\mathrm{max}$')
 
@@ -432,7 +435,7 @@ def create_ax1():
     fig_subp = [3, 1]
     fig1, ax1 = vf.my_plot(fig_wh, fig_subp)
         
-    fig_pos  = np.array([0.25, 0.71, 0.67, 0.27])
+    fig_pos  = np.array([0.24, 0.71, 0.68, 0.26])
     for i in np.arange(fig_subp[0]):
         ax1[i].set_position( fig_pos + np.array([0, -0.32*i, 0,  0]) )
 
