b6325fefd46bacdf221d98b9415c669f0d9cdf90
YIN <yin@YINs-iMac.local>
2023-04-19 21:25:26 +0800

    fig2

--- myvasp/vasp_epi_fit.py
+++ myvasp/vasp_epi_fit.py
@@ -43,6 +43,8 @@ class class_epi_fit:
         fname1 = 'lepi_res_ntrain_%s.pkl'  %(fname_suffix)
         self.calc_sdUss_tilde(filename=fname1, islip = islip) 
         self.plot_lepi_res_ntrain(filename=fname1, sd=sd) 
+        if fname_suffix == 'ssf':
+            self.plot_lepi_res_ntrain_2(filename=fname1) 
 
         print('==> scan dmax')
         dmax_max = np.min([ self.lpairs.shellmax, 11 ])
@@ -191,7 +193,7 @@ class class_epi_fit:
         ax1[2].legend( fontsize=6, loc='lower left' )       
 
         ax1[2].set_xlim( fig_xlim )
-        ax1[2].set_xlabel('$n_\\mathrm{train}$')
+        ax1[2].set_xlabel('$N_\\mathrm{train}$')
 
         ax1[0].set_ylim([-0.06, 0.04]) 
         # ax1[1].set_ylim([-0.02, 0.02])      
@@ -203,7 +205,7 @@ class class_epi_fit:
         
 
         vf.confirm_0( dmax.std() )
-        str1 = 'EPI $d_\\mathrm{max}=d_{%d}$' %( dmax[0] ) 
+        str1 = 'EPI $N_d = %d$' %( dmax[0] ) 
         vf.my_text(ax1[0], str1, 0.5, 0.9, ha='center' )
 
         create_abc(ax1)
@@ -211,6 +213,54 @@ class class_epi_fit:
         figname = '%s.pdf' %(filename[0:-4])
         plt.savefig(figname)
         plt.close('all')
+
+
+
+
+    def plot_lepi_res_ntrain_2(filename):      
+        lepi_res = vf.my_read_pkl(filename) 
+        
+        dmax   = np.array([])
+        ntrain = np.array([])
+        
+        Vnm = np.zeros(lepi_res[0].dmax)   # a specific pair nm 
+        
+        for i in np.arange( len(lepi_res) ):
+            dmax   = np.append( dmax,   lepi_res[i].dmax   )  
+            ntrain = np.append( ntrain, lepi_res[i].ntrain )  
+            
+            Vnm = np.vstack([Vnm, lepi_res[i].epi[:,0,1]])
+        Vnm=np.delete(Vnm, 0, 0) 
+        
+        
+        ax1 = create_ax2() 
+        fig_xlim = [0, ntrain[-1]]
+        print(fig_xlim) 
+
+        for j in np.arange(Vnm.shape[1]):
+            str1 = '$d_{%d}$'  %(j+1)
+            ax1.plot( ntrain, Vnm[:,j], '-', label=str1 )        
+        ax1.plot( fig_xlim, [0, 0], '--', color='gray' ) 
+        
+
+        ax1.legend( fontsize=6, loc='lower left', ncol=2)            
+        
+        ax1.set_xlim( fig_xlim )
+        ax1.set_xlabel('$N_\\mathrm{train}$')
+        
+        ax1.set_ylim([-0.06, 0.04]) 
+        ax1.set_ylabel('EPI $V_{\\mathrm{AuNi}, d}$ (eV)')           
+        
+
+        vf.confirm_0( dmax.std() )          
+        str1 = 'EPI $N_d = %d$' %( dmax[0] ) 
+        vf.my_text(ax1, str1, 0.5, 0.9, ha='center' )
+        
+        figname = '%s_2.pdf' %(filename[0:-4])
+        plt.savefig(figname)
+        plt.close('all')
+
+
 
 
 
@@ -283,15 +333,15 @@ class class_epi_fit:
 
         ax1[2].set_xlim( fig_xlim )
 
-        xt  = ax1[2].get_xticks()
-        xtl = ax1[2].get_xticklabels()
-        for i in np.arange(len(xt)):
-            str1 = '$ d_{%d} $'  %( xt[i] ) 
-            xtl[i] = str1 
-        ax1[2].set_xticks(xt) 
-        ax1[2].set_xticklabels(xtl) 
+        # xt  = ax1[2].get_xticks()
+        # xtl = ax1[2].get_xticklabels()
+        # for i in np.arange(len(xt)):
+        #     str1 = '$ d_{%d} $'  %( xt[i] ) 
+        #     xtl[i] = str1 
+        # ax1[2].set_xticks(xt) 
+        # ax1[2].set_xticklabels(xtl) 
 
-        ax1[2].set_xlabel('$d_\\mathrm{max}$')
+        ax1[2].set_xlabel('$N_d$')
 
         ax1[0].set_ylim([-0.06, 0.04]) 
         ax1[1].set_ylim([0, 1]) 
@@ -299,10 +349,10 @@ class class_epi_fit:
 
         ax1[0].set_ylabel('EPI $V_{\\mathrm{AuNi}, d}$ (eV)')
         ax1[1].set_ylabel('percent error')
-        ax1[2].set_ylabel('theoretical $ \\widetilde{\\sigma}_{\\Delta U_{s-s}} $ (eV)')
+        ax1[2].set_ylabel('analytical $ \\widetilde{\\sigma}_{\\Delta U_{s-s}} $ (eV)')
 
 
-        str1 = 'EPI $n_\\mathrm{train}=%d$' %( ntrain[0] ) 
+        str1 = 'EPI $N_\\mathrm{train} = %d$' %( ntrain[0] ) 
         vf.my_text(ax1[0], str1, 0.5, 0.9, ha='center' )
 
         create_abc(ax1)
@@ -326,6 +376,18 @@ def create_ax1():
     fig_pos  = np.array([0.24, 0.71, 0.68, 0.26])
     for i in np.arange(fig_subp[0]):
         ax1[i].set_position( fig_pos + np.array([0, -0.32*i, 0,  0]) )
+
+    return ax1 
+
+
+
+def create_ax2():
+    fig_wh = [3, 2.2]
+    fig_subp = [1, 1]
+    fig1, ax1 = vf.my_plot(fig_wh, fig_subp)
+        
+    fig_pos  = np.array([0.24, 0.18, 0.68, 0.75])
+    ax1.set_position( fig_pos )
 
     return ax1 
 
